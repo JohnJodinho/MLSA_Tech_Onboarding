09cc9f25e12311b56d3ff9f3f21068132a268838
John Ibrahim <101939269+JohnJodinho@users.noreply.github.com>
2023-08-27 12:45:19 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,16 +24,17 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "John Ibrahim",
+  title: "Microsoft Power Platform Developer",
+  email: "jodinjzzy@gmail.com",
+  gitHub: "JohnJodinho",
+  instagram: "akjodinho",
+  linkedIn: "john-ibrahim-08aa81190",
   medium: "",
-  twitter: "microsoft",
+  twitter: "Jodinho97",
   youTube: "Code",
 };
+
 
 const primaryColor = "#4E567E";
 const secondaryColor = "#D2F1E4";
